5ba649c89448fe74d4ce65eff6885420cb4ff9c7
Jean Pierre <73299769+PSevenTech@users.noreply.github.com>
2022-09-10 19:47:04 -0300

    DEDSEC
    made by PSevenTech
    instagram:@jeanpseven

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # everyscripts
-You can install a lot of repo. Termux/Kali...    command :git clone https://github.com/PSevenTech/everyscripts 
+You can install a lot of repo. Termux/Kali...    command :git clone https://github.com/PSevenTech/DEDSEC 
 if the github information appears asking for username and password give your github login information 
+
+Instagram:@jeanpseven
+Github:PSevenTech
---
+++ ds.sh
@@ -0,0 +1,95 @@
+cd $HOME 
+cd .. 
+cd usr && cd etc 
+rm motd
+rm -r motd
+rm -vf motd
+clear
+git clone PSevenTech/everyscripts/motd
+git clone PSevenTech/everyscripts/motd.txt
+rename motd.txt motd
+
+cd $HOME
+clear
+echo "\e[32m[*..]Installing\e[0m"
+clear 
+echo "\e[32m[.*.]Installing\e[0m"
+clear 
+echo "\e[32m[..*]Installing\e[0m"
+clear 
+echo "\e[32m[.*.]Installing\e[0m"
+clear 
+echo "\e[32m[*..]Installing\e[0m"
+clear 
+echo "\e[32m[.*.]Installing\e[0m"
+clear 
+echo "\e[32m[..*]Installing\e[0m"
+clear 
+echo "\e[32m[.*.]Installing\e[0m"
+clear 
+
+pkg update
+pkg remove game-repo -y
+pkg remove science-repo -y
+pkg update -y && pkg upgrade -y
+git clone git@github.com:company/*.git 
+git clone https://github.com/wuseman/WBRUTE
+git clone https://github.com/vanhauser-thc/thc-hydra
+cd thc-hydra
+curl  -O https://raw.githubusercontent.com/Gameye98/Gameye98.github.io/master/wordlist/password.txt
+./configure
+make
+make install
+cd ..
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/m4ll0k/Infoga
+curl  -O https://raw.githubusercontent.com/Gameye98/Gameye98.github.io/master/scripts/androzenmap.sh
+git clone https://github.com/Hornegam/BruteForce
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/jseidl/GoldenEye
+git clone https://github.com/baraalmasri/xerxes
+git clone https://github.com/Gameye98/Black-Hydra
+git clone https://github.com/Slayeri4/instahack
+git clone https://github.com/geovedi/indonesian-wordlist
+curl  -O https://raw.githubusercontent.com/Gameye98/Gameye98.github.io/master/scripts/facebook3.py
+curl  -O https://raw.githubusercontent.com/Gameye98/Gameye98.github.io/master/wordlist/password.txt
+git clone https://github.com/themastersunil/ngrok
+git clone https://github.com/st42/termux-sudo
+git clone https://github.com/Neo-Oli/termux-ubuntu
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+git clone https://github.com/Moham3dRiahi/XAttacker
+apt install root-repo ettercap
+git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/Lexiie/Termux-Apktool
+git clone https://github.com/derv82/wifite
+git clone https://github.com/wifiphisher/wifiphisher
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/ExpertAnonymous/PhoneInfoga
+git clone https://github.com/guruku/BTC-to-IDR-checker
+git clone https://github.com/Magentron/chkrootkit
+git clone https://github.com/gameye98/Lazymux
+git clone https://github.com/Rajnudusad/Tool-X
+git clone https://github.com/kancotdiq/ipcs
+git clone https://github.com/Manisso/fsociety
+git clone https://github.com/Night420/Termux-Fix
+git clone https://github.com/googleinurl/scanner-inurl
+git clone https://github.com/samay827/samayphish2
+git clone https://github.com/issamelgerkh/userrecon.git
+git clone https://github.com/pembriahmad/DDOS
+git clone https://github.com/TSMaitry/VirusX
+git clone https://github.com/CyberXCodder/XerXes.git
+git clone https://github.com/termuxprofessor/insunfollow
+git clone https://github.com/th3unkn0r/osi.ig
+git clone https://github.com/techchipnet/CamPhish
+git clone https://github.com/DarkSecDeveloper/HiddenEye.git
+git clone https://github.com/Datalux/Osintgram
+git clone https://github.com/lulz3xploit/LittleBrother
+git clone https://github.com/arismelachroinos/lscript
+git clone https://github.com/dronekit/dronekit-python
+git clone https://github.com/dhondta/dronesploit
+clear
+echo -e "\e[32m[+] Script created by P.\e[0m";
+echo -e "\e[32m[+] and playboyhacker insta:@pplayboyyhacker & @jeanpseven\e[0m";
+
+                                                                               
